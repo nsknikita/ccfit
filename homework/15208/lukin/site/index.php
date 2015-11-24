94116616191494a2dd1b438e1c0d95a7c1ff1af5
2ZeroSix <2ZeroSix1@gmail.com>
2015-11-24 20:07:07 +0700

    Update index.php

--- homework/15208/lukin/site/index.php
+++ homework/15208/lukin/site/index.php
@@ -1,6 +1,6 @@
 <?php
 	file_put_contents("include/.count", $count = file_get_contents("include/.count") + 1);
-	echo "Вы $count посетитель<BR>";
+	echo "ц╥ц≥ $count ц░ц▐ц⌠ц┘ц■ц┴ц■ц┘ц▄ц≤<BR>";
 	$inip = $_SERVER['REMOTE_ADDR'];
 	$ip =  file("include/.iplist");
 	$cip = 0;
@@ -10,35 +10,16 @@
 	}
 	if ($cip > 0)
 	{
-		echo "Всего уникальных посетителей: $i<BR>Вы были $cip уникальным посетителем<BR>";
+		echo "ц╥ц⌠ц┘ц┤ц▐ ц∙ц▌ц┴ц▀ц│ц▄ц≤ц▌ц≥ц┬ ц░ц▐ц⌠ц┘ц■ц┴ц■ц┘ц▄ц┘ц┼: $i<BR>ц╥ц≥ ц┌ц≥ц▄ц┴ $cip ц∙ц▌ц┴ц▀ц│ц▄ц≤ц▌ц≥ц█ ц░ц▐ц⌠ц┘ц■ц┴ц■ц┘ц▄ц┘ц█<BR>";
 		$cip--;
-		echo "Ваш ip: $ip[$cip]<BR>";
+		echo "ц╥ц│ц⌡ ip: $ip[$cip]<BR>";
 	}
 	else
 	{
 		$i++;
-		echo "Вы $i уникальный посетитель<BR>Ваш ip: $inip<BR>";
+		echo "ц╥ц≥ $i ц∙ц▌ц┴ц▀ц│ц▄ц≤ц▌ц≥ц┼ ц░ц▐ц⌠ц┘ц■ц┴ц■ц┘ц▄ц≤<BR>ц╥ц│ц⌡ ip: $inip<BR>";
 		$ipfile = fopen("include/.iplist", 'a');
 		fwrite($ipfile, "$inip\n");
 		fclose($ipfile);
 	}
-/*	$iplist = fopen("include/.ip", 'r+');
-	$work = 0;
-	$cip = 0;
-	fgets($iplist);
-	while ($work == 0) {
-		if(!($ip = fgets($iplist))) {$work = 1}
-		$cip++;
-		if(strcmp("$ip","$inip") == 0) {$work = 2}
-	}
-	if (work == 1) {
-		echo "Вы $cip уникальный пользователь.\n";
-		fwrite($iplist, "\n$ip");
-	}
-	else
-	{
-	echo "Вы были $cip уникальным пользователем"
-	}
-	fclose($iplist);
-*/
 ?>
