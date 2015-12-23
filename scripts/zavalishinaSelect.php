21a275ef1ff2e24ee05e2143e814570e595e44d1
Zavalishina Elena <olesandra2797@ngs.ru>
2015-12-23 14:32:12 +0600

    modified select.php

--- scripts/zavalishinaSelect.php
+++ scripts/zavalishinaSelect.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php 
-
-if (!file_exists("text.txt")) { 
-echo "File not found."; 
-} 
-else { 
-$file = fopen("text.txt", "r"); 
-} 
-?> 
-
-<select> 
-<?php while ($str = fgets($file)) { 
-echo "<option>" . $str . "</option>"; 
-}; 
-?> 
-=======
 <?php
   if (!file_exists("text.txt")) {
     echo "File not found.";
@@ -30,5 +13,4 @@ while ($str = fgets($file)) {
   echo "<option>" . $str . "</option>";
 };
 ?>
->>>>>>> bed98be3c172310e67b0756f15012ca55a6f61e7
 </select>
